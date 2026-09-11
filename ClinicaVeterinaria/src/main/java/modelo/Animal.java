be791ce1fd063cae496537fc4636ab70e7b7da45
Sariasm1 <sariasm1@unicartagena.edu.co>
2026-09-10 23:20:42 -0500

    HerenciaAnimal 2

--- ClinicaVeterinaria/src/main/java/modelo/Animal.java
+++ ClinicaVeterinaria/src/main/java/modelo/Animal.java
@@ -11,5 +11,48 @@ package modelo;
 public abstract class Animal {
     private String nombre;
     private int edad;
+    private String color;
+
+    public Animal(String nombre, int edad, String color) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.color = color;
+    }
+
+    public Animal() {
+    }
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "nombre=" + nombre + ", edad=" + edad + ", color=" + color + '}';
+    }
+    
+    
     
 }
